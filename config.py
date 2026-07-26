9dfad9d52d3bb6583a9aba0ec75e3a0fd5097913
vennela <rajeswarimake2701@gmail.com>
2026-07-26 15:22:58 +0530

    Prepare project for Render deployment

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
-class Config:
-    SECRET_KEY = "finsight123"
+import os
 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://username:password@localhost/finsight_project"
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "finsight123")
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "mysql+pymysql://username:password@localhost/finsight_project"
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
