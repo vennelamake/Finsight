4f1093d3dc365fb290e1f2770490a93297582e52
vennela <rajeswarimake2701@gmail.com>
2026-07-26 15:45:49 +0530

    Your commit message

--- config.py
+++ config.py
@@ -3,9 +3,13 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "finsight123")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv(
+    DATABASE_URL = os.getenv(
         "DATABASE_URL",
         "mysql+pymysql://username:password@localhost/finsight_project"
     )
 
+    if DATABASE_URL.startswith("mysql://"):
+        DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+pymysql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
